ad35667949e8902f24f71d23d6cfc198a81ff995
Fernando Duran <fduran@pneumatech.biz>
2025-04-15 17:21:50 -0500

    Updated images and homepage text ver2

--- data/index.ts
+++ data/index.ts
@@ -28,7 +28,7 @@ export const gridItems = [
   },
   {
     id: 3,
-    title: "My tech stack",
+    title: "my tech skills",
     description: "I constantly try to improve",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
